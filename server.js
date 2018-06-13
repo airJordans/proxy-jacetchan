bf3b3964affb9eb849f88f68fcac6b39d21ecce8
Jace Chan <jacetchan@gmail.com>
2018-06-12 17:29:22 -0700

    Create proxy server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/listings/:id', express.static(path.join(__dirname, 'public')));
+
+app.get('/listings/:listingId/overviews', (req, res) => {
+    res.redirect(`http://localhost:3003${req.url}`);
+});
+
+app.get('/listings/:listingId/reviews', (req, res) => {
+    res.redirect(`http://localhost:3003${req.url}`);
+});
+
+// app.get('/listings/:listingId/reviews', (req, res) => {
+//   res.send('test');
+// });
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
