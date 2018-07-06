f5d2b3380149f4915a7fbf364f8e657115dc85d4
Kianna <kiannaquach.kq@gmail.com>
2018-07-06 12:11:09 -0700

    Finish proxy

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.use('/listings/:id', express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // handles review routes
 app.get('/listings/:listingId/overviews', (req, res) => {
-  res.redirect(`http://ec2-54-219-133-3.us-west-1.compute.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-18-221-134-157.us-east-2.compute.amazonaws.com${req.url}`);
 });
 
 app.get('/listings/:listingId/reviews', (req, res) => {
-  res.redirect(`http://ec2-54-219-133-3.us-west-1.compute.amazonaws.com${req.url}`);
+  res.redirect(`http://ec2-18-221-134-157.us-east-2.compute.amazonaws.com${req.url}`);
 });
 
 // handles booking routes
 app.get('/listings/:listingId/booking/core', (req, res) => {
-  res.redirect(`http://ec2-54-67-69-199.us-west-1.compute.amazonaws.com${req.url}`);
+  res.redirect(`http://18.188.144.184${req.url}`);
 });
 
 app.get('/listings/:listingId/booking/availability/', (req, res) => {
-  res.redirect(`http://ec2-54-67-69-199.us-west-1.compute.amazonaws.com${req.url}`);
-});
-
-// handles carousel routes
-app.get('/listings/:listingId/pictures', (req, res) => {
-  res.redirect(`http://ec2-18-208-170-149.compute-1.amazonaws.com${req.url}`);
+  res.redirect(`http://18.188.144.184${req.url}`);
 });
 
 // handles description routes
-
 app.get('/listings/:listingId/overview', (req, res) => {
-  res.redirect(`http://awslistingmodule-env.x9r2zr55dh.us-west-2.elasticbeanstalk.com${req.url}`);
+  res.redirect(`http://18.188.210.40${req.url}`);
 });
 
 app.get('/listings/:listingId/arrangements', (req, res) => {
-  res.redirect(`http://awslistingmodule-env.x9r2zr55dh.us-west-2.elasticbeanstalk.com${req.url}`);
+  res.redirect(`http://18.188.210.40${req.url}`);
 });
 
 app.listen(port, () => {
